ef939688bc9bcc6ba9fc8aad6a94362524d667e4
ShellCode33 <clement.fleury33@gmail.com>
2018-04-30 15:04:55 +0200

    Init project

---
+++ huffman.py
@@ -0,0 +1,11 @@
+
+class Huffman(object):
+
+    def __init__(self):
+        self.characters_found = []
+
+    def compress(self, bytes_list):
+        pass
+
+    def decompress(self, base64_string):
+        pass
