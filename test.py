195dc1ed43814dc5f0f1efae5f8f9e29b924ecbe
ShellCode33 <clement.fleury33@gmail.com>
2018-05-01 18:39:22 +0200

    Huffman compression done.

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
+#!/usr/bin/python3
+# coding: utf-8
+
 from huffman import *
-from tree import *
 
 if __name__ == "__main__":
     huffman = Huffman()
-    huffman.compress(b"Coucou")
-    print(huffman.characters_frequency)
-    tree = Tree()
-    print(tree.is_leaf())
